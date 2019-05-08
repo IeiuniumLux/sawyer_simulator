4347acb26ed195167fae39a0016b8917392e04d5
IeiuniumLux <cagiva851@gmail.com>
2019-05-07 18:48:24 -0700

    aws script

--- aws_sawyer_sim.sh
+++ aws_sawyer_sim.sh
@@ -23,8 +23,6 @@ colcon bundle
 
 aws s3 mb s3://sawyer-bucket-robomaker
 
-aws s3api put-object --bucket sawyer-bucket-robomaker --key outputs/
-
 aws s3 cp ~/environment/simulation_ws/bundle/output.tar s3://sawyer-bucket-robomaker/sawyer_sim.tar
 
 aws robomaker create-simulation-application --name sawyer_sim --sources s3Bucket=sawyer-bucket-robomaker,s3Key=sawyer_sim.tar,architecture=X86_64 --robot-software-suite name=ROS,version=Kinetic --simulation-software-suite name=Gazebo,version=7 --rendering-engine name=OGRE,version=1.x
